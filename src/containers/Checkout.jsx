bef5f3b07fb9eaa99764a29e59a337e7df37c8d5
SuichiM <MartiniRoqueEsteban@gmail.com>
2022-02-21 11:30:28 -0300

    state at the checkout

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,30 +1,44 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/components/Checkout.css';
 
+import AppContext from '../context/AppContext';
+
 const Checkout = () => {
+  const { state, removeFromCart } = useContext(AppContext);
+  const { cart } = state;
+  const handleRemove = (item) => removeFromCart(item);
+
+  const finalPrice = cart.reduce((price, item) => price + item.price, 0);
+  const EMPTY_CART = cart.length === 0;
+
   return (
     <div className="Checkout">
       <div className="Checkout-content">
-        <h3>Lista de Pedidos:</h3>
-        <div className="Checkout-item">
-          <div className="Checkout-element">
-            <h4>ITEM name</h4>
-            <span>$10</span>
+        {EMPTY_CART ? <h3>No hay productos ;(</h3> : <h3>Lista de Pedidos:</h3>}
+        {cart.map((item) => (
+          <div className="Checkout-item">
+            <div className="Checkout-element">
+              <h4>{item.title}</h4>
+              <span> ${item.price}</span>
+            </div>
+            <button type="button" onClick={() => handleRemove(item)}>
+              <i className="fas fa-trash-alt" />
+            </button>
           </div>
-          <button type="button">
-            <i className="fas fa-trash-alt" />
-          </button>
+        ))}
+      </div>
+
+      {!EMPTY_CART && (
+        <div className="Checkout-sidebar">
+          <h3>{`Precio Total: $${finalPrice}`}</h3>
+          <Link to="/checkout/information">
+            <button type="button">Continuar pedido</button>
+          </Link>
         </div>
-      </div>
-      <div className="Checkout-sidebar">
-        <h3>Precio Total: $10</h3>
-        <Link to="/checkout/information">
-          <button type="button">Continuar pedido</button>
-        </Link>
-      </div>
+      )}
     </div>
   );
-}
+};
 
 export default Checkout;
