4e430c43dc1dae7bbeaa399b1ef20f847364edc8
SuichiM <MartiniRoqueEsteban@gmail.com>
2022-02-21 10:58:33 -0300

    add context to handle the app state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/components/Header.css'
+import AppContext from '../context/AppContext';
+
+import '../styles/components/Header.css';
 
 const Header = () => {
+  const { state } = useContext(AppContext);
+
+  const itemsInCart = state.cart.length;
+
   return (
     <div className="Header">
       <h1 className="Header-title">
@@ -11,10 +17,11 @@ const Header = () => {
       <div className="Header-checkout">
         <Link to="/checkout">
           <i className="fas fa-shopping-basket" />
+          {itemsInCart && <div className="Header-alert">{itemsInCart}</div>}
         </Link>
       </div>
     </div>
   );
-}
+};
 
 export default Header;
